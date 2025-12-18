c6cafd21c316b28b97a6c6284b1b7e2f10887fc5
John Duggan <dugganjw@ornl.gov>
2025-12-18 10:22:27 -0500

    Stub out UI

--- src/exphub/app/views/css_status.py
+++ src/exphub/app/views/css_status.py
@@ -1,7 +1,9 @@
 """Module for the CSS Status tab."""
 
 from nova.trame.view.components import InputField
-from nova.trame.view.layouts import VBoxLayout
+from nova.trame.view.layouts import GridLayout, HBoxLayout, VBoxLayout
+from trame.widgets import plotly
+from trame.widgets import vuetify3 as vuetify
 
 from ..view_models.main import MainViewModel
 
@@ -16,5 +18,51 @@ class CSSStatusView:
         self.view_model.update_cssstatus_figure()
 
     def create_ui(self) -> None:
-        with VBoxLayout(columns=1):
-            InputField(v_model="model_cssstatus.plot_type", items="model_cssstatus.plot_type_options", type="select")
+        with VBoxLayout(classes="border-md mb-1 pa-1", stretch=True):
+            plotly.Figure()
+
+            with HBoxLayout():
+                InputField()
+                InputField(type="checkbox")
+
+            with HBoxLayout(gap="0.25em", valign="center"):
+                vuetify.VBtn("Detail")
+                vuetify.VBtn("Profiles")
+                vuetify.VBtn("Clear Array & Counts")
+                vuetify.VBtn("Reset ROI")
+                InputField()
+                InputField()
+                InputField()
+                InputField()
+                InputField()
+                InputField(type="checkbox")
+
+        with GridLayout(columns=3, gap="0.25em", stretch=True):
+            with VBoxLayout(classes="border-md pa-1", column_span=2):
+                with vuetify.VTabs(classes="mb-1", style="height: 40px !important;"):
+                    vuetify.VTab("TOF (All Modules)")
+                    vuetify.VTab("d-Space (All Modules)")
+                    vuetify.VTab("q-Space (All Modules)")
+                    vuetify.VTab("d-Space (ROI Filtered)")
+                    vuetify.VTab("q-Space (ROI Filtered)")
+
+                with HBoxLayout():
+                    vuetify.VBtn("Linear")
+                    vuetify.VSpacer()
+                    vuetify.VBtn("More Detail")
+
+                plotly.Figure()
+
+            with VBoxLayout(stretch=True):
+                with VBoxLayout(classes="border-md pa-1", stretch=True):
+                    plotly.Figure()
+                    vuetify.VBtn("Cursor Detail")
+
+                with GridLayout(classes="border-md pa-1", columns=2):
+                    InputField()
+                    InputField()
+                    InputField()
+                    InputField()
+                    InputField()
+                    InputField()
+                    vuetify.VLabel("Running")
